5353a39ac4763e018672dbe1720b4bdf8086fd4e
Matt Napier <matt.napier4@gmail.com>
2026-02-03 09:33:33 +0000

    Update ingest.js
    Removed duplicate createClient

--- api/ingest.js
+++ api/ingest.js
@@ -1,8 +1,6 @@
 import fetch from "node-fetch";
 import { createClient } from "@supabase/supabase-js";
 
-import { createClient } from "@supabase/supabase-js";
-
 const supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_KEY
